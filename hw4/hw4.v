e84e415fa0b96b169f30603d21081de1b93c77de
Pilar Luiz <pluiz@appliedinvention.com>
2025-03-14 14:00:42 -0700

    hw4 done

--- hw4/hw4.v
+++ hw4/hw4.v
@@ -13,7 +13,7 @@ module hw4(
   // First, define an array of 8-bit vectors to hold your message.
   // It should be at least 256 entries long.
 
-  // reg [7:0] ...
+  reg [7:0] memory[0:1023];
 
 
   // Now, initialize that array from a file using `$readmemh()`.
@@ -21,49 +21,67 @@ module hw4(
   // characters you want to send.  Use e.g. <https://www.rapidtables.com/convert/number/ascii-to-hex.html>
   // to create it.
 
-  // initial ...
+  initial $readmemh("hello.hex", memory);
 
 
   // We'll need two variables to define the state of our system, one (call
-  // it `tx_data`) to hold an index into our message, and the other (call it
+  // it `idx`) to hold an index into our message, and the other (call it
   // `running`) to indicate whether we're currently sending the message or not.
 
-  // reg ...
-
+  reg [9:0] idx = 0;
+  reg running = 0;
 
   // Now instantiate a `uart_tx` module like in hw3.
   // You'll want to declare `tx_data` as a reg, and
   // the other signals (`tx_valid`, `tx_ready`, `tx_busy`) as wires.
 
-  // uart_tx ...
+  reg [7:0] tx_data;
+  wire tx_valid, tx_ready, tx_busy;
+
+  uart_tx tx (
+    .clk(CLK),
+    .rst(1'b0),
+    .txd(TX),
+    .busy(tx_busy),
+    .data(tx_data),
+    .valid(tx_valid),
+    .ready(tx_ready),
+    .prescale(12000000 / (9600*8))
+  );
 
 
   // Let's assign `running` and `tx_busy` to LEDs so we can see what's happening.
 
-  // assign ...
-
+  assign LEDG_N = ~running;
+  assign LEDR_N = ~tx_busy;
 
   // Finally, our main logic!
 
   // First, let's read from memory into `tx_data` always on the *negative*
   // clock edge, so we don't have to worry about stall cycles.
 
-  // always @(negedge CLK) ...
+  always @(negedge CLK) begin
+    tx_data <= memory[idx];
+  end
 
 
   // Now, we can specify `tx_valid`: data is valid so long as we're running,
   // *and* the data we've read is non-zero.  (Array elements after the file we've loaded
   // will all be zero.)
 
-  // assign tx_valid = ...
+  assign tx_valid = running && |tx_data;
 
 
   // We need a way to detect a button click.
   // You can use your solution for this from hw2 here.
 
-  // reg btn_was_pressed;
-  // always ...
-  // wire btn_click = ...
+  reg btn;
+  reg last_btn;
+  always @(posedge CLK) begin
+    btn <= ~BTN_N;
+    last_btn <= btn;
+  end
+  wire btn_click = btn & ~last_btn;
 
 
   // Now our main logic!
@@ -77,7 +95,15 @@ module hw4(
   // (both `tx_valid` and `tx_ready` are true),
   // we should increment the index.
 
-  // always @(posedge CLK) ...
+  always @(posedge CLK) begin
+    if (running) begin
+      if (tx_valid && tx_ready) idx <= idx + 1;
+      if (tx_data == 0) running <= 0;
+    end else if (!running && btn_click) begin
+      idx <= 0;
+      running <= 1;
+    end
+  end
 
 endmodule
 
